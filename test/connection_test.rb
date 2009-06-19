9a8434272aceaed71d1cbd395639798a86dced0f
Sebastian Nowak <sebastian.nowak@gmail.com>
2009-06-19 12:05:43 +0200

    Test for Connection.go method

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -2,10 +2,26 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require 'test/unit'
 require 'connection'
+require 'rubygems'
+require 'fakeweb'
 
 class ConnectionTest < Test::Unit::TestCase
   def setup
     @flak_connection = Rflak::Connection.new
+  end
+
+
+  def test_fetch
+    FakeWeb.register_uri(:get,"http://api.flaker.pl/api/type:user/login:dstranz/limit:2", :string => fake_response_from_flaker)
+    response = Rflak::Connection.go('user') do |conn|
+      conn.login 'dstranz'
+      conn.limit 2
+    end
+    assert_kind_of(Array, response)
+    response.each do |entry|
+      assert_kind_of(Rflak::Entry, entry)
+      assert_kind_of(Rflak::User, entry.user)
+    end
   end
 
 
@@ -72,5 +88,15 @@ class ConnectionTest < Test::Unit::TestCase
 
   def test_comments
     assert_equal '/comments:false', @flak_connection.comments(false)
+  end
+
+
+  protected
+
+
+  def fake_response_from_flaker
+    <<END
+{"user":{"login":"dstranz","avatar":"http:\/\/static0.flaker.pl\/static\/images\/flaker\/user_generated\/u_139_1227549010.jpg_50.jpeg","url":"http:\/\/flaker.pl\/dstranz","sex":"m"},"entries":[{"id":"854874","title":"<!--deprecated-->","text":"<div class=\"etext\">Poczt\u00f3wka z #<a class=\"tag\" title=\"zobacz inne wpisy otagowane jako Bydgoszcz\" href=\"http:\/\/flaker.pl\/t\/bydgoszcz\">Bydgoszcz<\/a>.y. Ulica Focha, plac Teatralny, w tle Opera. <span class=\"original-link type_flaker\"><a href=\"\" title=\"\" class=\"link\" rel=\"nofollow\"><\/a> <\/span><\/div>","source":"flaker","data":{"images":[{"big":"m_139_1227962103_0_600.jpeg","small":"m_139_1227962103_0_75.jpeg","alt":""}]},”time”:”Sat, 29 Nov 2008 12:35:05 +0100″,”datetime”:”2008-11-29 12:35:05″,”timestamp”:”1227958505″,”permalink”:”http:\/\/flaker.pl\/f\/854874″,”link”:”",”has_photo”:”1″,”has_link”:”0″,”has_video”:”0″,”user”:{”login”:”dstranz”,”avatar”:”http:\/\/static0.flaker.pl\/static\/images\/flaker\/user_generated\/u_139_1227549010.jpg_50.jpeg”,”url”:”http:\/\/flaker.pl\/dstranz”,”sex”:”m”,”action”:”wrzuci\u0142 obrazek na Flakera”},”comments”:[]}]}
+END
   end
 end
